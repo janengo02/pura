697914a2d6da4c85b2e63e6424edd2c0345f8532
Jane Ngo <minhanh11200@gmail.com>
2023-11-21 17:51:36 +0900

    SplitPane

--- client/src/features/dashboard/Dashboard.jsx
+++ client/src/features/dashboard/Dashboard.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Container } from '@chakra-ui/react'
 import Navbar from './Navbar'
+import SplitPane from './SplitPane'
 // import t from '../../lang/i18n'
 
 const Dashboard = () => {
    return (
       <>
          <Navbar />
-         <Container bg='white' w='full' maxW='100vw' h='100vh'>
-            hello
+         <Container bg='white' w='full' maxW='100vw' h='100vh' p={0}>
+            <SplitPane>
+               <SplitPane.Left />
+               <SplitPane.Right />
+            </SplitPane>
          </Container>
       </>
    )
---
+++ client/src/features/dashboard/SplitPane.jsx
@@ -0,0 +1,94 @@
+import React, {
+   createContext,
+   useState,
+   useRef,
+   createRef,
+   useEffect,
+   useContext
+} from 'react'
+import { Box, Flex, Divider } from '@chakra-ui/react'
+
+const splitPaneContext = createContext()
+
+export default function SplitPane({ children, ...props }) {
+   const [leftWidth, setLeftWidth] = useState(null)
+   const separatorXPosition = useRef(null)
+
+   const splitPaneRef = createRef()
+
+   const onMouseDown = (e) => {
+      separatorXPosition.current = e.clientX
+   }
+
+   const onMouseMove = (e) => {
+      if (!separatorXPosition.current) {
+         return
+      }
+
+      const newLeftWidth = leftWidth + e.clientX - separatorXPosition.current
+      separatorXPosition.current = e.clientX
+
+      setLeftWidth(newLeftWidth)
+   }
+
+   const onMouseUp = () => {
+      separatorXPosition.current = null
+   }
+
+   useEffect(() => {
+      document.addEventListener('mousemove', onMouseMove)
+      document.addEventListener('mouseup', onMouseUp)
+
+      return () => {
+         document.removeEventListener('mousemove', onMouseMove)
+         document.removeEventListener('mouseup', onMouseUp)
+      }
+   })
+
+   return (
+      <Flex w='full' h='full' {...props} ref={splitPaneRef}>
+         <splitPaneContext.Provider value={{ leftWidth, setLeftWidth }}>
+            {children[0]}
+
+            <Divider
+               orientation='vertical'
+               w={3}
+               bg='gray.100'
+               cursor='col-resize'
+               border='none'
+               onMouseDown={onMouseDown}
+            />
+            {children[1]}
+         </splitPaneContext.Provider>
+      </Flex>
+   )
+}
+
+SplitPane.Left = function SplitPaneLeft(props) {
+   const leftRef = createRef()
+   const { leftWidth, setLeftWidth } = useContext(splitPaneContext)
+
+   useEffect(() => {
+      if (!leftWidth) {
+         setLeftWidth(leftRef.current.clientWidth)
+         leftRef.current.style.flex = 'none'
+         return
+      }
+
+      leftRef.current.style.width = `${leftWidth}px`
+   }, [leftWidth])
+
+   return (
+      <Box w='full' h='full' {...props} ref={leftRef}>
+         heelo
+      </Box>
+   )
+}
+
+SplitPane.Right = function SplitPaneRight(props) {
+   return (
+      <Box w='full' h='full' {...props}>
+         Hello
+      </Box>
+   )
+}
