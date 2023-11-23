0266bd577f9e77cfce02dc57190fc61501926d63
Jane Ngo <jane.ngo@jg-corporation.com>
2023-11-23 23:45:03 +0900

    fix splipane provider bug

--- client/src/features/dashboard/Dashboard.jsx
+++ client/src/features/dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Container } from '@chakra-ui/react'
 import Navbar from './Navbar'
-import SplitPane from './SplitPane'
+import SplitPane, {
+   PageDivider,
+   SplitPaneLeft,
+   SplitPaneRight
+} from './SplitPane'
 // import t from '../../lang/i18n'
 
 const Dashboard = () => {
@@ -10,8 +14,9 @@ const Dashboard = () => {
          <Navbar />
          <Container bg='white' w='full' maxW='100vw' h='100vh' p={0}>
             <SplitPane>
-               <SplitPane.Left />
-               <SplitPane.Right />
+               <SplitPaneLeft />
+               <PageDivider />
+               <SplitPaneRight />
             </SplitPane>
          </Container>
       </>
--- client/src/features/dashboard/SplitPane.jsx
+++ client/src/features/dashboard/SplitPane.jsx
@@ -1,5 +1,4 @@
 import React, {
-   createContext,
    useState,
    useRef,
    createRef,
@@ -7,36 +6,44 @@ import React, {
    useContext
 } from 'react'
 import { Box, Flex, Divider } from '@chakra-ui/react'
+import SplitPaneContext from '../../context/SplitPaneContext'
+const leftRef = createRef()
+const rightRef = createRef()
 
-const splitPaneContext = createContext()
-
-export default function SplitPane({ children, ...props }) {
+const SplitPane = ({ children, ...props }) => {
    const [leftWidth, setLeftWidth] = useState(null)
+   const [focusDivider, setFocusDivider] = useState(false)
    const separatorXPosition = useRef(null)
 
    const onMouseDown = (e) => {
       separatorXPosition.current = e.clientX
+      setFocusDivider(true)
+      // Prevent text selection while resizing
+      rightRef.current.style.userSelect = 'none'
+      leftRef.current.style.userSelect = 'none'
    }
 
    const onMouseMove = (e) => {
       if (!separatorXPosition.current) {
          return
       }
-
       const newLeftWidth = leftWidth + e.clientX - separatorXPosition.current
       separatorXPosition.current = e.clientX
-
       setLeftWidth(newLeftWidth)
+      setFocusDivider(true)
    }
 
    const onMouseUp = () => {
       separatorXPosition.current = null
+      // Enable text selection after resizing
+      rightRef.current.style.userSelect = 'auto'
+      leftRef.current.style.userSelect = 'auto'
+      setFocusDivider(false)
    }
 
    useEffect(() => {
       document.addEventListener('mousemove', onMouseMove)
       document.addEventListener('mouseup', onMouseUp)
-
       return () => {
          document.removeEventListener('mousemove', onMouseMove)
          document.removeEventListener('mouseup', onMouseUp)
@@ -45,26 +52,65 @@ export default function SplitPane({ children, ...props }) {
 
    return (
       <Flex w='full' h='full' {...props}>
-         <splitPaneContext.Provider value={{ leftWidth, setLeftWidth }}>
-            {children[0]}
-
-            <Divider
-               orientation='vertical'
-               w={3}
-               bg='gray.200'
-               cursor='col-resize'
-               border='none'
-               onMouseDown={onMouseDown}
-            />
-            {children[1]}
-         </splitPaneContext.Provider>
+         <SplitPaneContext.Provider
+            value={{
+               leftWidth,
+               setLeftWidth,
+               onMouseDown,
+               focusDivider,
+               setFocusDivider
+            }}
+         >
+            {children}
+         </SplitPaneContext.Provider>
       </Flex>
    )
 }
 
-SplitPane.Left = function SplitPaneLeft(props) {
-   const leftRef = createRef()
-   const { leftWidth, setLeftWidth } = useContext(splitPaneContext)
+export const PageDivider = () => {
+   const { onMouseDown, focusDivider, setFocusDivider } =
+      useContext(SplitPaneContext)
+   const dividerRef = createRef()
+   useEffect(() => {
+      const dividerNode = dividerRef.current
+      let timeout = null
+
+      const startFocusDivider = (event) => {
+         if (dividerNode) {
+            if (dividerNode.contains(event.target)) {
+               // Change divider color after hovering for 0.2s
+               timeout = setTimeout(() => setFocusDivider(true), 200)
+            } else {
+               setFocusDivider(false)
+            }
+         }
+      }
+
+      const removeFocusDivider = () => {
+         clearTimeout(timeout)
+         setFocusDivider(false)
+      }
+
+      dividerNode.addEventListener('mouseover', startFocusDivider)
+      dividerNode.addEventListener('mouseout', removeFocusDivider)
+      return () => {
+         document.removeEventListener('mouseover', startFocusDivider)
+      }
+   }, [dividerRef, setFocusDivider])
+   return (
+      <Divider
+         orientation='vertical'
+         cursor='col-resize'
+         borderWidth={4}
+         borderColor={focusDivider ? 'purple.500' : 'gray.200'}
+         onMouseDown={onMouseDown}
+         ref={dividerRef}
+         flexShrink={0}
+      />
+   )
+}
+export const SplitPaneLeft = (props) => {
+   const { leftWidth, setLeftWidth } = useContext(SplitPaneContext)
 
    useEffect(() => {
       if (!leftWidth) {
@@ -72,21 +118,28 @@ SplitPane.Left = function SplitPaneLeft(props) {
          leftRef.current.style.flex = 'none'
          return
       }
-
+      // TODO: Switch to % instead of px
       leftRef.current.style.width = `${leftWidth}px`
-   }, [leftRef, leftWidth, setLeftWidth])
+   }, [leftWidth, setLeftWidth])
 
    return (
       <Box w='full' h='full' {...props} ref={leftRef}>
-         heelo
+         Kanban board
       </Box>
    )
 }
+export const SplitPaneRight = (props) => {
+   const { leftWidth } = useContext(SplitPaneContext)
 
-SplitPane.Right = function SplitPaneRight(props) {
+   useEffect(() => {
+      rightRef.current.style.flex = 'none'
+      rightRef.current.style.width = `calc(100vw - ${leftWidth}px)`
+   }, [leftWidth])
+
    return (
-      <Box w='full' h='full' {...props}>
-         Hello
+      <Box w='full' h='full' {...props} ref={rightRef}>
+         Calendar
       </Box>
    )
 }
+export default SplitPane
