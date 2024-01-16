66cc2979ce59a45424791fb86d190ce9d846b23d
Jane Ngo <minhanh11200@gmail.com>
2024-01-16 17:07:53 +0900

    api: delete group

--- routes/api/group.js
+++ routes/api/group.js
@@ -5,6 +5,7 @@ const { check, validationResult } = require('express-validator')
 
 const Page = require('../../models/Page')
 const Group = require('../../models/Group')
+const Task = require('../../models/Task')
 
 // @route   POST api/group/new/:page-id
 // @desc    Create a new group
@@ -109,6 +110,16 @@ router.post('/update/:page_id/:group_id', [auth], async (req, res) => {
       })
    }
 
+   //   Validation: Check if group exists
+   const group = await Group.findById(req.params.group_id)
+   if (!group) {
+      return res.status(404).json({
+         errors: [
+            { code: '404', title: 'alert-oops', msg: 'alert-group-notfound' }
+         ]
+      })
+   }
+
    //   Validation: Form input
    const result = validationResult(req)
    if (!result.isEmpty()) {
@@ -146,6 +157,112 @@ router.post('/update/:page_id/:group_id', [auth], async (req, res) => {
          ])
          .populate('group_order', ['title', 'color', 'visibility'])
          .populate('tasks', ['title', 'schedule'])
+
+      res.json(newPage)
+   } catch (error) {
+      console.error('---ERROR---: ' + error.message)
+      res.status(500).json({
+         errors: [
+            { code: '500', title: 'alert-oops', msg: 'alert-server_error' }
+         ]
+      })
+   }
+})
+
+// @route   DELETE api/group/:page-id/:group-id
+// @desc    Delete a group
+// @access  Private
+router.delete('/:page_id/:group_id', [auth], async (req, res) => {
+   //   Validation: Check if page exists
+   const page = await Page.findById(req.params.page_id)
+   if (!page) {
+      return res.status(404).json({
+         errors: [
+            { code: '404', title: 'alert-oops', msg: 'alert-page-notfound' }
+         ]
+      })
+   }
+
+   //   Validation: Check if user is the owner
+   if (page.user.toString() !== req.user.id) {
+      return res.status(401).json({
+         errors: [
+            { code: '401', title: 'alert-oops', msg: 'alert-user-unauthorize' }
+         ]
+      })
+   }
+
+   //   Validation: Check if group exists
+   const group = await Group.findById(req.params.group_id)
+   if (!group) {
+      return res.status(404).json({
+         errors: [
+            { code: '404', title: 'alert-oops', msg: 'alert-group-notfound' }
+         ]
+      })
+   }
+
+   //   Prepare: Set up new tasks array
+   var newTasks = page.tasks
+   const { group_id } = req.params
+   const groupIndex = page.group_order.indexOf(group_id)
+   const progressCount = page.progress_order.length
+   const mapStart = progressCount * groupIndex
+   const mapEnd = mapStart + progressCount - 1
+
+   //   Prepare: Set up new group_order
+   var newGroupOrder = page.group_order
+   newGroupOrder.splice(groupIndex, 1)
+
+   //   Prepare: Set up new task_map
+   var newTaskMap = page.task_map
+   var taskCount = newTaskMap[mapEnd]
+   if (mapStart !== 0) {
+      taskCount = newTaskMap[mapEnd] - newTaskMap[mapStart - 1]
+   }
+   for (let i = mapEnd + 1; i < newTaskMap.length; i++) {
+      newTaskMap[i] -= taskCount
+   }
+   newTaskMap.splice(mapStart, mapEnd - mapStart + 1)
+
+   try {
+      // Data: Delete tasks
+      if (newTaskMap[mapStart] !== newTaskMap[mapEnd]) {
+         newTaskStart = newTaskMap[mapStart]
+         newTaskEnd = newTaskMap[mapEnd] - 1
+
+         for (let i = newTaskStart; i <= newTaskEnd.length; i++) {
+            deletedTaskId = newTasks[i]
+            await Task.deleteOne({ _id: deletedTaskId })
+         }
+         newTasks.splice(newTaskStart, newTaskEnd - newTaskStart + 1)
+      }
+
+      // Data: Delete group
+      await group.deleteOne()
+
+      // Data: Add new group to page
+      const newPage = await Page.findOneAndUpdate(
+         { _id: req.params.page_id },
+         {
+            $set: { group_order: newGroupOrder },
+            $set: { tasks: newTasks },
+            $set: { update_date: new Date() }
+         },
+         { new: true }
+      )
+         .populate('progress_order', [
+            'title',
+            'title_color',
+            'color',
+            'visibility'
+         ])
+         .populate('group_order', ['title', 'color', 'visibility'])
+         .populate('tasks', ['title', 'schedule'])
+
+      // Data: Update page's task_map
+      newPage.task_map = newTaskMap
+      newPage.save()
 
       res.json(newPage)
    } catch (error) {
