1550b3ab7e847f205345689a71c1438c89a68315
Jane Ngo <jane.ngo@jg-corporation.com>
2024-01-24 00:07:47 +0900

    initiate task card modal

--- client/src/features/dashboard/kanban/task/TaskCard.jsx
+++ client/src/features/dashboard/kanban/task/TaskCard.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 import {
+   Button,
    Card,
    Flex,
    IconButton,
@@ -10,6 +11,13 @@ import {
    MenuButton,
    MenuItem,
    MenuList,
+   Modal,
+   ModalBody,
+   ModalCloseButton,
+   ModalContent,
+   ModalFooter,
+   ModalHeader,
+   ModalOverlay,
    Spacer,
    Text,
    useDisclosure
@@ -34,6 +42,8 @@ const TaskCard = ({
    const [hovered, setHovered] = useState(false)
    const [editing, setEditing] = useState(false)
    const dropdownMenu = useDisclosure()
+   const modalMenu = useDisclosure()
+   const modalCard = useDisclosure()
    const delTask = () => {
       const formData = {
          page_id: page_id,
@@ -58,63 +68,152 @@ const TaskCard = ({
       setEditing(false)
    })
    return (
-      <Draggable draggableId={draggableId} index={index}>
-         {(provided, snapshot) => (
-            <Card
-               {...provided.draggableProps}
-               {...provided.dragHandleProps}
-               ref={provided.innerRef}
-               variant='outline'
-               boxShadow={snapshot.isDragging ? 'md' : undefined}
-               p={2}
-               w='full'
-               marginBottom={1}
-               onMouseEnter={(e) => {
-                  e.preventDefault()
-                  setHovered(true)
-               }}
-               onMouseLeave={(e) => {
-                  e.preventDefault()
-                  setHovered(false)
-               }}
-            >
-               <Flex>
-                  {task.schedule.length > 0 ? (
-                     <Text fontSize='xs' color='gray.500'>
-                        {t('schedule_status-true')}
-                     </Text>
-                  ) : (
-                     <Text fontSize='xs' color='red.500'>
-                        {t('schedule_status-false')}
-                     </Text>
-                  )}
-                  <Spacer />
+      <>
+         <Draggable draggableId={draggableId} index={index}>
+            {(provided, snapshot) => (
+               <Card
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  ref={provided.innerRef}
+                  variant='outline'
+                  boxShadow={snapshot.isDragging ? 'md' : undefined}
+                  bg={hovered ? 'gray.50' : undefined}
+                  p={2}
+                  w='full'
+                  marginBottom={1}
+                  onMouseEnter={(e) => {
+                     e.preventDefault()
+                     setHovered(true)
+                  }}
+                  onMouseLeave={(e) => {
+                     e.preventDefault()
+                     setHovered(false)
+                  }}
+               >
+                  <Flex cursor='pointer'>
+                     {task.schedule.length > 0 ? (
+                        <Text
+                           fontSize='xs'
+                           color='gray.500'
+                           onClick={modalCard.onOpen}
+                        >
+                           {t('schedule_status-true')}
+                        </Text>
+                     ) : (
+                        <Text
+                           fontSize='xs'
+                           color='red.500'
+                           onClick={modalCard.onOpen}
+                        >
+                           {t('schedule_status-false')}
+                        </Text>
+                     )}
+
+                     <Spacer onClick={modalCard.onOpen} />
+                     <Menu
+                        isLazy
+                        isOpen={dropdownMenu.isOpen}
+                        onClose={dropdownMenu.onClose}
+                     >
+                        <MenuButton
+                           as={IconButton}
+                           icon={<PiDotsThreeBold />}
+                           variant='ghost'
+                           size='xs'
+                           colorScheme='gray'
+                           color='gray.600'
+                           opacity={hovered || dropdownMenu.isOpen ? 1 : 0}
+                           onClick={dropdownMenu.onOpen}
+                        ></MenuButton>
+                        <MenuList>
+                           <MenuItem
+                              icon={<PiPencilLine size={18} />}
+                              fontSize='sm'
+                              onClick={async (e) => {
+                                 e.preventDefault()
+                                 setEditing(true)
+                              }}
+                           >
+                              {t('btn-edit-name')}
+                           </MenuItem>
+                           <MenuItem
+                              icon={<PiTrash size={18} />}
+                              fontSize='sm'
+                              onClick={async (e) => {
+                                 e.preventDefault()
+                                 delTask()
+                              }}
+                           >
+                              {t('btn-delete-task')}
+                           </MenuItem>
+                        </MenuList>
+                     </Menu>
+                  </Flex>
+                  <Flex
+                     h={6}
+                     alignItems='center'
+                     overflow='hidden'
+                     cursor='pointer'
+                     onClick={modalCard.onOpen}
+                  >
+                     {editing ? (
+                        <FormProvider {...methods} h='fit-content'>
+                           <form noValidate autoComplete='on'>
+                              <MultiInput
+                                 name='title'
+                                 type='text'
+                                 variant='unstyled'
+                                 placeholder={t('placeholder-untitled')}
+                                 validation={s.name}
+                                 defaultValue={task.title}
+                                 fontWeight={600}
+                                 borderRadius={0}
+                                 autoFocus
+                                 onFocus={async (e) => {
+                                    e.preventDefault()
+                                    e.currentTarget.select()
+                                 }}
+                                 onBlur={async (e) => {
+                                    e.preventDefault()
+                                    onBlur()
+                                 }}
+                              />
+                           </form>
+                        </FormProvider>
+                     ) : (
+                        <Text color='gray.600' fontWeight={600}>
+                           {task.title}
+                        </Text>
+                     )}
+                  </Flex>
+               </Card>
+            )}
+         </Draggable>
+         <Modal
+            isOpen={modalCard.isOpen}
+            onClose={modalCard.onClose}
+            size='2xl'
+            scrollBehavior='inside'
+            blockScrollOnMount={false}
+         >
+            <ModalOverlay />
+            <ModalContent>
+               <ModalHeader display='flex' justifyContent='flex-end'>
                   <Menu
                      isLazy
-                     isOpen={dropdownMenu.isOpen}
-                     onClose={dropdownMenu.onClose}
+                     isOpen={modalMenu.isOpen}
+                     onClose={modalMenu.onClose}
                   >
                      <MenuButton
                         as={IconButton}
-                        icon={<PiDotsThreeBold />}
+                        icon={<PiDotsThreeBold size={20} />}
                         variant='ghost'
                         size='xs'
                         colorScheme='gray'
                         color='gray.600'
-                        opacity={hovered || dropdownMenu.isOpen ? 1 : 0}
-                        onClick={dropdownMenu.onOpen}
+                        onClick={modalMenu.onOpen}
                      ></MenuButton>
                      <MenuList>
-                        <MenuItem
-                           icon={<PiPencilLine size={18} />}
-                           fontSize='sm'
-                           onClick={async (e) => {
-                              e.preventDefault()
-                              setEditing(true)
-                           }}
-                        >
-                           {t('btn-edit-name')}
-                        </MenuItem>
                         <MenuItem
                            icon={<PiTrash size={18} />}
                            fontSize='sm'
@@ -127,41 +226,32 @@ const TaskCard = ({
                         </MenuItem>
                      </MenuList>
                   </Menu>
-               </Flex>
-               <Flex h={6} alignItems='center' overflow='hidden'>
-                  {editing ? (
-                     <FormProvider {...methods} h='fit-content'>
-                        <form noValidate autoComplete='on'>
-                           <MultiInput
-                              name='title'
-                              type='text'
-                              variant='unstyled'
-                              placeholder={t('placeholder-untitled')}
-                              validation={s.name}
-                              defaultValue={task.title}
-                              fontWeight={600}
-                              borderRadius={0}
-                              autoFocus
-                              onFocus={async (e) => {
-                                 e.preventDefault()
-                                 e.currentTarget.select()
-                              }}
-                              onBlur={async (e) => {
-                                 e.preventDefault()
-                                 onBlur()
-                              }}
-                           />
-                        </form>
-                     </FormProvider>
-                  ) : (
-                     <Text color='gray.600' fontWeight={600}>
-                        {task.title}
-                     </Text>
-                  )}
-               </Flex>
-            </Card>
-         )}
-      </Draggable>
+               </ModalHeader>
+               <ModalBody>
+                  <FormProvider {...methods} h='fit-content'>
+                     <form noValidate autoComplete='on'>
+                        <MultiInput
+                           name='title'
+                           type='text'
+                           variant='unstyled'
+                           placeholder={t('placeholder-untitled')}
+                           validation={s.name}
+                           defaultValue={task.title}
+                           fontWeight={600}
+                           borderRadius={0}
+                           fontSize='2xl'
+                           onBlur={async (e) => {
+                              e.preventDefault()
+                              onBlur()
+                           }}
+                        />
+                     </form>
+                  </FormProvider>
+               </ModalBody>
+               <ModalFooter></ModalFooter>
+            </ModalContent>
+         </Modal>
+      </>
    )
 }
 
