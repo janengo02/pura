6a1ecec035e3b87a1ec0161f22d289c66ed696df
Jane Ngo <jane.ngo@jg-corporation.com>
2024-06-10 23:10:33 +0900

    fix sync condition

--- client/src/features/dashboard/calendar/event/EventWrapper.jsx
+++ client/src/features/dashboard/calendar/event/EventWrapper.jsx
@@ -30,8 +30,8 @@ const EventWrapper = ({
    syncedEventLoading
 }) => {
    const initRef = useRef()
-   const taskIndex = page.tasks.findIndex(
-      (t) => t.google_events.includes(event.id) && event.title === t.title
+   const taskIndex = page.tasks.findIndex((t) =>
+      t.google_events.includes(event.id)
    )
 
    const taskId = taskIndex !== -1 ? page.tasks[taskIndex]._id : null
