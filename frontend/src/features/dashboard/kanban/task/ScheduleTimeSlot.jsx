747cf8128243cef3f8e780d49ef6d59e68d00aaa
Jane Ngo <minhanh11200@gmail.com>
2025-07-10 23:02:38 +0900

    Remove create google event button

--- frontend/src/features/dashboard/kanban/task/ScheduleTimeSlot.jsx
+++ frontend/src/features/dashboard/kanban/task/ScheduleTimeSlot.jsx
@@ -12,30 +12,20 @@ import { createSelector } from 'reselect'
 
 // Actions
 import { updateTaskAction } from '../../../../actions/taskActions'
-import { createGoogleEventAction } from '../../../../actions/googleAccountActions'
 
 // UI Components
-import {
-   Flex,
-   IconButton,
-   Input,
-   Menu,
-   MenuButton,
-   MenuItem,
-   MenuList,
-   Tooltip
-} from '@chakra-ui/react'
+import { Flex, IconButton, Input } from '@chakra-ui/react'
 
 // External Libraries
 import cloneDeep from 'clone-deep'
 
 // Utils & Icons
 import { useReactiveTranslation } from '../../../../hooks/useReactiveTranslation'
-import { PiCalendarPlusFill, PiTrash } from 'react-icons/pi'
 import { stringToDateTimeLocal } from '../../../../utils/dates'
 
 // Hooks
 import useLoading from '../../../../hooks/useLoading'
+import { PiTrash } from 'react-icons/pi'
 
 // =============================================================================
 // MAIN COMPONENT
@@ -47,8 +37,7 @@ const ScheduleTimeSlot = React.memo(
       index,
       // Redux props
       updateTaskAction,
-      createGoogleEventAction,
-      scheduleData: { task, pageId, googleAccounts }
+      scheduleData: { task, pageId }
    }) => {
       // -------------------------------------------------------------------------
       // SCHEDULE UPDATE HANDLERS
@@ -102,22 +91,6 @@ const ScheduleTimeSlot = React.memo(
       const [deleteSlot, deleteSlotLoading] = useLoading(handleDeleteSlot)
 
       // -------------------------------------------------------------------------
-      // GOOGLE CALENDAR HANDLERS
-      // -------------------------------------------------------------------------
-
-      const handleCreateGoogleEvent = useCallback(
-         async (accountId) => {
-            await createGoogleEventAction({
-               target_task: task,
-               slot_index: index,
-               page_id: pageId,
-               account_id: accountId
-            })
-         },
-         [createGoogleEventAction, task, index, pageId]
-      )
-
-      // -------------------------------------------------------------------------
       // UI EVENT HANDLERS
       // -------------------------------------------------------------------------
 
@@ -143,14 +116,6 @@ const ScheduleTimeSlot = React.memo(
             await deleteSlot()
          },
          [deleteSlot]
-      )
-
-      const handleGoogleAccountClick = useCallback(
-         (accountId) => async (e) => {
-            e.preventDefault()
-            await handleCreateGoogleEvent(accountId)
-         },
-         [handleCreateGoogleEvent]
       )
 
       // -------------------------------------------------------------------------
@@ -243,43 +208,6 @@ const ScheduleTimeSlot = React.memo(
          [deleteSlotLoading, handleDeleteClick]
       )
 
-      const googleCalendarMenu = useMemo(
-         () => (
-            <Tooltip
-               hasArrow
-               label={t('tooltip-time_slot-view_calendar')}
-               placement='bottom'
-            >
-               <Menu isLazy closeOnSelect={false}>
-                  <MenuButton
-                     as={IconButton}
-                     icon={<PiCalendarPlusFill size={16} />}
-                     variant='ghost'
-                     colorScheme='gray'
-                     color='text.primary'
-                     size='sm'
-                  />
-                  <MenuList zIndex={10}>
-                     {googleAccounts.map((account) => (
-                        <MenuItem
-                           key={account._id}
-                           display='flex'
-                           justifyContent='space-between'
-                           alignItems='center'
-                           gap={1}
-                           fontSize='xs'
-                           onClick={handleGoogleAccountClick(account.accountId)}
-                        >
-                           {account.accountEmail}
-                        </MenuItem>
-                     ))}
-                  </MenuList>
-               </Menu>
-            </Tooltip>
-         ),
-         [googleAccounts, handleGoogleAccountClick, t]
-      )
-
       // -------------------------------------------------------------------------
       // RENDER LOGIC
       // -------------------------------------------------------------------------
@@ -294,7 +222,6 @@ const ScheduleTimeSlot = React.memo(
          >
             {startTimeInput}-{endTimeInput}
             {deleteButton}
-            {googleCalendarMenu}
          </Flex>
       )
    }
@@ -315,11 +242,9 @@ ScheduleTimeSlot.propTypes = {
    }).isRequired,
    index: PropTypes.number.isRequired,
    updateTaskAction: PropTypes.func.isRequired,
-   createGoogleEventAction: PropTypes.func.isRequired,
    scheduleData: PropTypes.shape({
       task: PropTypes.object.isRequired,
-      pageId: PropTypes.string.isRequired,
-      googleAccounts: PropTypes.array.isRequired
+      pageId: PropTypes.string.isRequired
    }).isRequired
 }
 
@@ -328,15 +253,10 @@ ScheduleTimeSlot.propTypes = {
 // =============================================================================
 
 const selectScheduleData = createSelector(
-   [
-      (state) => state.task.task,
-      (state) => state.page._id,
-      (state) => state.googleAccount.googleAccounts
-   ],
-   (task, pageId, googleAccounts) => ({
+   [(state) => state.task.task, (state) => state.page._id],
+   (task, pageId) => ({
       task,
-      pageId,
-      googleAccounts
+      pageId
    })
 )
 
@@ -349,8 +269,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-   updateTaskAction,
-   createGoogleEventAction
+   updateTaskAction
 }
 
 // =============================================================================
