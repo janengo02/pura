a71673c42e91394dda27ec02943d63fbdafe3387
Jane Ngo <minhanh11200@gmail.com>
2025-08-21 16:57:31 +0900

    fix: update MongoDB connection string variable from MONGO_URI to DATABASE_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const connectDB = async () => {
    try {
-      await mongoose.connect(process.env?.MONGO_URI)
+      await mongoose.connect(process.env?.DATABASE_URI)
       console.log('MongoDB Connected...')
    } catch (error) {
       console.error('MongoDB Connection Error: ', error.message)
