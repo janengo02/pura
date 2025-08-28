82022fdfbd5d7238e280543924e583bdf33bc1b4
Jane Ngo <minhanh11200@gmail.com>
2025-08-29 08:08:58 +0900

    feat: Remove loading state management from PrivateRoute and delete loadingSlice

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -2,25 +2,20 @@ import React from 'react'
 import { Navigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { Spinner } from '@chakra-ui/react'
 
 const PrivateRoute = ({
-   isLoading,
    component: Component,
    auth: { isAuthenticated }
 }) => {
-   if (isLoading) return <Spinner />
-   if (!isLoading && isAuthenticated) return <Component />
+   if (isAuthenticated) return <Component />
    return <Navigate to='/login' />
 }
 
 PrivateRoute.propTypes = {
-   isLoading: PropTypes.bool.isRequired,
    auth: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state) => ({
-   isLoading: state.loading.isLoading,
    auth: state.auth
 })
 
